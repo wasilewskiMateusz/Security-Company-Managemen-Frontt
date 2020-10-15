9389d62e2e643d05d3c98b989c12d4a271e29ba1
Mateusz Wasilewski <wasilewskimateusz1998@gmail.com>
2020-10-16 00:20:25 +0200

    Add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './auth/components/login/login.component';
 
 const routes: Routes = [
-  { path: '',   redirectTo: 'login', pathMatch: 'full' },
+  //{ path: '',   redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'login',
     component: LoginComponent
