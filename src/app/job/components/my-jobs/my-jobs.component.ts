6db484b850f7f9ca8a1b1e6fe873280400ffcd05
Mateusz Wasilewski <wasilewskimateusz1998@gmail.com>
2020-11-29 15:00:17 +0100

    Add info about no contract

--- src/app/job/components/my-jobs/my-jobs.component.ts
+++ src/app/job/components/my-jobs/my-jobs.component.ts
@@ -4,6 +4,7 @@ import {JobService} from '../../services/job.service';
 import {ContractService} from '../../services/contract.service';
 import {SuccessHandler} from '../../../share/success-handler';
 import {Contract} from '../../models/contract';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-my-jobs',
@@ -18,7 +19,8 @@ export class MyJobsComponent implements OnInit {
   constructor(private jobService: JobService,
               private router: Router,
               private contractService: ContractService,
-              private successHandler: SuccessHandler) {
+              private successHandler: SuccessHandler,
+              private snackBar: MatSnackBar) {
   }
 
   ngOnInit(): void {
@@ -30,6 +32,13 @@ export class MyJobsComponent implements OnInit {
     this.userId = JSON.parse(atob(token.split('.')[1])).id;
     this.jobService.getMyContracts(this.userId).subscribe(res => {
       this.contracts = res;
+      if (this.contracts.length === 0) {
+        this.snackBar.open('You have no contracts', '', {
+          duration: 4000,
+          verticalPosition: 'top',
+          panelClass: ['warn-snackbar']
+        });
+      }
     });
   }
 
