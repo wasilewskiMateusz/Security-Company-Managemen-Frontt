fac2dc85e627787f0793fd03eb5a3bf72d156636
Mateusz Wasilewski <wasilewskimateusz1998@gmail.com>
2020-12-30 18:09:39 +0100

    Fix

--- src/app/share/error-handler.ts
+++ src/app/share/error-handler.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Observable, throwError} from 'rxjs';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -21,19 +22,26 @@ export class ErrorHandlerService {
 
       errorMessage = error.error.message;
 
+      this.snackBar.open(errorMessage, '', {
+        duration: 4000,
+        verticalPosition: 'top',
+        panelClass: ['error-snackbar']
+      });
+
     } else {
-
-      // server-side error
-
       errorMessage = error.error.message;
+
+
+      if (error.status !== 403 && error.status !== 404 && error.status !== 500) {
+        this.snackBar.open(errorMessage, '', {
+          duration: 4000,
+          verticalPosition: 'top',
+          panelClass: ['error-snackbar']
+        });
+      }
 
     }
 
-    this.snackBar.open(errorMessage, '', {
-      duration: 4000,
-      verticalPosition: 'top',
-      panelClass: ['error-snackbar']
-    });
 
     return throwError(error);
 
